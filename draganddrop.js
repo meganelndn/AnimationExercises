37a474bff17513c083cc0553e93d69f9e7dff482
megane <megane.londono@hotmail.com>
2020-03-11 13:21:26 +0100

    drag and drop working

--- draganddrop.js
+++ draganddrop.js
@@ -3,10 +3,20 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    test.addEventListener("mousemove", theMouseMove);
+    svg.addEventListener("mousedown", startDrag);
+    svg.addEventListener("mousemove", drag);
+    svg.addEventListener("mouseup", endDrag);
 }
 
-function theMouseMove(evt) {
+function startDrag(evt) {
+    //svg.addEventListener("mousemove", drag);
+}
+
+function drag(evt) {
     evt.target.setAttribute("cx", evt.pageX / window.innerWidth * 500);
     evt.target.setAttribute("cy", evt.pageY / window.innerHeight * 500 * (window.innerHeight / window.innerWidth));
+}
+
+function endDrag(evt) {
+    svg.removeEventListener("mousemove", drag);
 }
